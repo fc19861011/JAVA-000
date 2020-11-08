5a1f7f2ace89a192cf675308b96d66adeb27e36e
383782840@qq.com <fc7976908>
2020-11-08 21:59:31 +0800

    organize file directory

--- Week_03/threadTest/ThreadTest.java
+++ Week_03/threadTest/ThreadTest.java
@@ -1,3 +1,33 @@
 public class ThreadTest {
-    
+    public static void main(String[] args) {
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Thread t = Thread.currentThread();
+                System.out.println("当前线程:"+t.getName());
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.setName("test-thread-1");
+        thread.setDaemon(false);
+        thread.start();
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread t = Thread.currentThread();
+            System.out.println("当前线程:"+t.getName());
+        });
+        thread2.setName("Lambda Thread");
+        thread2.start();
+        System.out.println("main thread finished");
+    }
 }
