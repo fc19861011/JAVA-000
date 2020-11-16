a83b868b78ed20682424b5b4789544900fa53491
383782840@qq.com <fc7976908>
2020-11-16 23:34:23 +0800

    jvm md

--- Week_01/homework/one/TestJavaByteCode.java
+++ Week_01/homework/one/TestJavaByteCode.java
@@ -1,12 +1,13 @@
-package homework.one;
-
 public class TestJavaByteCode {
 
     private final String f = "final";
-
     public static String b = "static";
+    private volatile String v = "volatile";
+
+    private int THREAD_ACTIVE_COUNT = 2;
 
     public void main(String[] args) {
+        v = "111";
         int n1 = 4;
         int n2 = 5;
         int n3 = n1 + n2;
@@ -21,6 +22,21 @@ public class TestJavaByteCode {
         System.out.println(d3);
         this.doLoop();
         System.out.println(f + b);
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            v = "222";
+        }).start();
+        System.out.println("1======: "+v);
+        while(Thread.activeCount() > THREAD_ACTIVE_COUNT) {
+            // 等待线程执行结束
+        }
+        System.out.println("2======: "+v);
+        
     }
 
     public void doLoop() {
