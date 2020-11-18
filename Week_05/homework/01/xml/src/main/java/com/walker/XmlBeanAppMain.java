0a7721bd66ade91c098533a686769a5906099118
傅超 <383782840@qq.com>
2020-11-18 22:55:47 +0800

    add homework for week05

---
+++ Week_05/homework/01/xml/src/main/java/com/walker/XmlBeanAppMain.java
@@ -0,0 +1,13 @@
+package com.walker;
+
+/**
+ * Hello world!
+ *
+ */
+public class XmlBeanAppMain
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Week_05/homework/01/xml/src/test/java/com/walker/XmlBeanAppMainTest.java
@@ -0,0 +1,20 @@
+package com.walker;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple XmlBeanAppMain.
+ */
+public class XmlBeanAppMainTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
