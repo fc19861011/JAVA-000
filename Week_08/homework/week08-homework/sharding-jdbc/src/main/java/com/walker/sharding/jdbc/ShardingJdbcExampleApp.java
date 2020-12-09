bc313010149946a7b6c98701d65c066714be03b1
fcwalker <383782840@qq.com>
2020-12-10 01:00:39 +0800

    commit week08 homework

---
+++ Week_08/homework/week08-homework/sharding-jdbc/src/main/java/com/walker/sharding/jdbc/ShardingJdbcExampleApp.java
@@ -0,0 +1,15 @@
+package com.walker.sharding.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class ShardingJdbcExampleApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcExampleApp.class);
+    }
+
+}
